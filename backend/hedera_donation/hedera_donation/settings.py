74e0f417e8f9514320a6bcba00a30037c7acd000
duysy <duyduysysy@gmail.com>
2025-02-17 22:48:10 +0700

    update

--- backend/hedera_donation/hedera_donation/settings.py
+++ backend/hedera_donation/hedera_donation/settings.py
@@ -87,9 +87,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "/static/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "/media/"
+
+STATIC_ROOT = 'static'
+STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
--- backend/hedera_donation/hedera_donation/urls.py
+++ backend/hedera_donation/hedera_donation/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
 from donation_app.api import api
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('api/', api.urls),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
